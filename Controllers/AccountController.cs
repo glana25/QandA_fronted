d678a54a9ee229986f7cea1c996ef9724f2f53bb
Vitalie Stirbu <stirbuvitalie@yahoo.com>
2020-05-01 21:09:32 -0500

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QandA_lesson1.DataStore;
+using QandA_lesson1.Models;
+
+namespace QandA_lesson1.Controllers
+{
+    public class AccountController : Controller
+    {
+        // .../account/signin
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        public IActionResult Admin()
+        {
+            var db = new DbHandler();
+            var users = db.GetUsers();
+
+            return View(users);
+        }
+
+        [HttpPost]
+        public IActionResult Authorize(QandA_lesson1.Models.User user)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult SignOut()
+        {
+            return View();
+        }
+    }
+}
---
+++ DataStore/DbHandler.cs
@@ -0,0 +1,20 @@
+﻿using QandA_lesson1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QandA_lesson1.DataStore
+{
+    public class DbHandler
+    {
+        public List<User> GetUsers()
+        {
+            return new List<User>()
+            {
+                new User{ Username = "john", Password= "john"},
+                new User { Username = "alex", Password = "admin"}
+            };
+        }
+    }
+}
