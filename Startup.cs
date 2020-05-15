dd745aa01fb7b9d9028c53184c6303634af5e17f
Vitalie Stirbu <stirbuvitalie@yahoo.com>
2020-05-15 18:04:45 -0500

    Add ask a question button

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace QandA_lesson1
             services.AddDbContext<QandAContext>(options =>
                             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
