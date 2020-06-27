850080b67266bdacb6cb69e81d16ff8a88263e05
Svitlana <svita.german@gmail.com>
2020-06-26 20:58:13 -0400

    Hot fix

--- Models/AnswersModel.cs
+++ Models/AnswersModel.cs
@@ -15,14 +15,9 @@ namespace QandA_lesson1.Models
         public string QuestionTitle { get; set; }
         public string QuestionUsername { get; set; }
         public DateTime QuestionDateCreated { get; set; }
-<<<<<<< HEAD
 
         [Required]
-        [StringLength(500, MinimumLength = 5)]
-=======
-        [Required]
         [StringLength(500)]
->>>>>>> 0c048e398dcd67d860d27cb9e1d6df87868cd79a
         public string Answer { get; set; }
         public List<Answer> QuestionAnswers { get; set; }
     }
--- Models/AskQuestionModel.cs
+++ Models/AskQuestionModel.cs
@@ -5,18 +5,10 @@ namespace QandA_lesson1.Models
     public class AskQuestionModel
     {
         [Required]
-<<<<<<< HEAD
-        [StringLength(100,MinimumLength =5)]
-        public string Title { get; set; }
-
-        [Required]
-        [StringLength(100, MinimumLength = 5)]
-=======
         [StringLength(100)]
         public string Title { get; set; }
         [Required]
         [StringLength(200)]
->>>>>>> 0c048e398dcd67d860d27cb9e1d6df87868cd79a
         public string Content { get; set; }
     }
 }
